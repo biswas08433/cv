248548e74448d2d27678420a206b0aa43b2406e5
biswas08433 <BISWAS08433@GMAIL.COM>
2023-09-23 00:27:27 +0530

    scaffold

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,11 @@
-let frame1 = 0
-let frame2 = 0
-let descriptionTextLine1 = "I am a Computer Scientist and Engineer, good at Systems programming and Networking."
-let descriptionTextLine2 = "Occationally I do some Game and GPU programming in free time."
-let descriptionLine1 = $(".description-block-line-1")
-let descriptionLine2 = $(".description-block-line-2")
+let frame1 = 0;
+let frame2 = 0;
+let descriptionTextLine1 = "I am a Computer Scientist and Engineer, good at Systems programming and Networking.";
+let descriptionTextLine2 = "Occationally I do some Game and GPU programming in free time.";
+let descriptionLine1 = $(".description-block-line-1");
+let descriptionLine2 = $(".description-block-line-2");
 
-let speed = 40
+let speed = 40;
 
 function typeWriter(element, text, frame) {
     if (frame < text.length) {
@@ -15,12 +15,6 @@ function typeWriter(element, text, frame) {
     }
 }
 
-typeWriter(descriptionLine1, descriptionTextLine1, frame1)
-typeWriter(descriptionLine2, descriptionTextLine2, frame2)
-// Check for click events on the navbar burger icon
-$(".navbar-burger").click(function () {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
+typeWriter(descriptionLine1, descriptionTextLine1, frame1);
+typeWriter(descriptionLine2, descriptionTextLine2, frame2);
 
-});
